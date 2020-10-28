204066bbca83738585d2437ad9f3cbde21cb0192
David Huebner <dhuebner@bushelpowered.com>
2020-10-28 12:10:12 -0500

    Update cross-site settings

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :majority_finder, MajorityFinderWeb.Endpoint,
   url: [host: "majorityfinder.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  check_origin: ["https://themajority.com"]
+  check_origin: ["//themajority.live", "//majorityfinder.gigalixirapp.com"]
 
 # Do not print debug messages in production
 config :logger, level: :info
